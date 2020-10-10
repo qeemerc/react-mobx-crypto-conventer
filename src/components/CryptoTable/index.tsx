a81e42deedceacab5b12ae58afef003806f00296
Qeemerc <qeemerc@yandex.ru>
2020-10-10 17:56:39 +0300

    add CryptoTable logic with CurrenciesStore

--- src/components/CryptoTable/index.tsx
+++ src/components/CryptoTable/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { observer, inject } from 'mobx-react'
 
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
@@ -7,40 +8,72 @@ import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+import { TCurrency } from '../../types'
+import CurrenciesStore from '../../stores/CurrenciesStore'
 
 import useStyles from './styles'
 
-const CryptoTable = () => {
-  const styles: any = useStyles()
+const UPDATE_INTERVAL = 30
+
+interface ICryptoTable {
+  currenciesStore?: CurrenciesStore
+}
+
+function CryptoTable ({ currenciesStore }: ICryptoTable) {
+  const styles = useStyles()
+  const currencies: TCurrency[] = currenciesStore!.getItems
+  const isFetching: Boolean = currenciesStore!.checkFetching
+
+  React.useEffect(() => {
+    currenciesStore!.fetchCurrencies()
+
+    const interval = setInterval(() => {
+      currenciesStore!.fetchCurrencies()
+    }, UPDATE_INTERVAL * 1000)
+
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <TableContainer
       component={Paper}
       className={styles.table}
     >
-      <Table className={styles.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>FullName</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Price</TableCell>
-            <TableCell>24h volume</TableCell>
-          </TableRow>
-        </TableHead>
+      {isFetching ? (
+        <div className={styles.preloader}>
+          <CircularProgress />
+        </div>
+      ) : (
+        <Table className={styles.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>FullName</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>24h volume</TableCell>
+            </TableRow>
+          </TableHead>
 
-        <TableBody>
-          <TableRow>
-            <TableCell>1</TableCell>
-            <TableCell>2</TableCell>
-            <TableCell>3</TableCell>
-            <TableCell>4</TableCell>
-            <TableCell>5</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+          <TableBody>
+            {currencies.map((currency: TCurrency) => (
+              <TableRow key={currency.name}>
+                <TableCell>
+                  <img src={currency.imageUrl} />
+                </TableCell>
+                <TableCell>{currency.name}</TableCell>
+                <TableCell>{currency.fullName}</TableCell>
+                <TableCell>{`$${currency.price}`}</TableCell>
+                <TableCell>{`$${currency.volume24Hour.toFixed(2)}`}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </TableContainer>
   )
 }
 
-export default CryptoTable
+export default inject('currenciesStore')(observer(CryptoTable))
--- src/stores/CurrenciesStore.ts
+++ src/stores/CurrenciesStore.ts
@@ -1,15 +1,47 @@
 import { observable, computed, action } from 'mobx'
 
-import { TCoin } from '../types'
+import { TCurrency } from '../types'
 
 export const CURRENCIES_NAME = 'CURRENCIES'
 
 class CurrenciesStore {
-  @observable currencies: TCoin[] = []
+  @observable private currencies: TCurrency[] = []
+  @observable private isFetching = false
 
-  constructor (initialCurrencies: TCoin[] = []) {
-    this.currencies = initialCurrencies
+  @computed
+  get getItems (): TCurrency[] {
+    return this.currencies
   }
+
+  @computed
+  get checkFetching (): Boolean {
+    return this.isFetching
+  }
+
+  @action
+  async fetchCurrencies () {
+    this.isFetching = true
+
+    const LIMIT = 10
+    const TSYM = 'USD'
+    const url = `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=${LIMIT}&tsym=${TSYM}`
+    const json = await fetch(url).then(res => res.json())
+    const currencies = await json.Data.map((coin: any) => {
+      const result: TCurrency = {
+        name: coin.CoinInfo.Name,
+        fullName: coin.CoinInfo.FullName,
+        imageUrl: `https://www.cryptocompare.com/${coin.CoinInfo.ImageUrl}`,
+        price: coin.RAW.USD.PRICE,
+        volume24Hour: coin.RAW.USD.VOLUME24HOUR
+      }
+
+      return result
+    })
+
+    this.currencies = currencies
+    this.isFetching = false
+  }
+
 }
 
 export default CurrenciesStore
